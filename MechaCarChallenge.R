2720fc91c000d235d29ebdd20ed889aa93904c8c
Soniaprogram <soniashar95@gmail.com>
2021-04-10 02:19:04 -0400

    Adding Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Determine the p-value and r-squared value for linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechcar)) #generate summary statistics
 
+
+# Deliverable 2
+# Import and read in Suspension_Coil.csv file as a table
+suspensioncoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a total_summary dataframe to get the mean, median, variance, and standard deviation
+# of the suspension coil's PSI column
+total_summary <- suspensioncoil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# Create a lot_summary dataframe and group each manufacturing lot by mean, median, variance,
+# and standard deviation of suspension coil's PSI column
+lot_summary <- suspensioncoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
