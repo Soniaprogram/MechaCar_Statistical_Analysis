1e86888c215a5e86f43bdb5fe9ea1b949d614a35
Soniaprogram <soniashar95@gmail.com>
2021-04-10 01:57:16 -0400

    Adding Deliverable 1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,10 +2,11 @@
 # Load the dplyr package
 library(dplyr)
 # Import and read in the MechaCar_mpg.csv file as a dataframe
-mechcar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+mechcar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 # Perform linear regression
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechcar_table) #generate multiple linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechcar) #generate multiple linear regression model
 
 # Determine the p-value and r-squared value for linear regression model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechcar_table)) #generate summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechcar)) #generate summary statistics
+
